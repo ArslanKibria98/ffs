802b48a60292632be21291b27025c5fd93839718
Saif Ali <116622210+saiksaif@users.noreply.github.com>
2024-07-11 15:46:51 +0500

    Added project update

--- src/views/page.jsx
+++ src/views/page.jsx
@@ -1,11 +1,12 @@
 // import "../globals.css"
 import React, { useEffect } from 'react'
 
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { SET_USER_INFO } from "../redux/store/auth";
 
 export default function AuthHandover() {
   const dispatch = useDispatch();
+  const userId = useSelector((state) => state?.authStore?.id);
   const token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IkI3OEQ5NkNDNEJFQkVGRTM3RjA5OERFN0YzQTU0NTM5IiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE3MTkzMDkyMTcsImV4cCI6MTcxOTMxMjgxNywiaXNzIjoiaHR0cHM6Ly9maW5vdmFzZWMueGludGRldi5jb206MzQ0MyIsImF1ZCI6Imh0dHBzOi8vZmlub3Zhc2VjLnhpbnRkZXYuY29tOjM0NDMvcmVzb3VyY2VzIiwiY2xpZW50X2lkIjoiOGJkYzExZDQtZGVkMS00MzAyLTk2ZjQtYzdlN2RhYzg2NDllIiwic3ViIjoiYWM5Yzc5OTQtM2RjNS00ZjhhLTkzOWItZjViZjdhZGQwNzY4IiwiYXV0aF90aW1lIjoxNzE5MzA5MjE3LCJpZHAiOiJsb2NhbCIsInJvbGUiOiJUZW5hbnQgQWRtaW4iLCJ1c2VySWQiOiJIYWlkZXJBbGlKb3lpYSIsImp0aSI6IjNGQzU4MEM4RjI3OEZCODBGRDM5MTcyMDg0MkNCNDIxIiwiaWF0IjoxNzE5MzA5MjE3LCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwiV29ya2Zsb3dzIiwib2ZmbGluZV9hY2Nlc3MiXSwiYW1yIjpbInB3ZCJdfQ.JwWi1ammBb5c9J7gbObQJJIToZsT-uNemtx6KhqHw_sPA-IbHOPioNGaxr0m3oy0UFZvSrQ6gZZwXmsyj-Be7k3ZTjyvu88WH_4gS_8ikGp67L85CutAuMOjSc5iuBJaCOdUbXo6pJOd6EzwQQwVd8oMe7Z6Nju9YMZ00cvmhnWPD0X7t-3fqdF5Ckb-pCJyMhX8RmHXWGtSYHzU2gAu-WBUxuk3s-SoVYXBB5GC-xJzsacBVPzJ3Wtr7eatMdcHQD7CcRtjGHVyeaFT-jSMso2dvi9B39-lWDovfpBeSUycMUmT0AkZWtlsxGAD4yGprxpLYfTiKUnM1hAgI6FhIA"
 
   function decodeAccessToken(token) {
@@ -23,13 +24,18 @@ export default function AuthHandover() {
           email: 'dummy@dummy.com',
           role: decodedToken?.payload?.role || 'dummy',
           nickname: 'dummy',
-          id: 'A2DEC207-EDFA-4619-BCF1-6DF55A5DD56F',
+          id: "A2DEC207-EDFA-4619-BCF1-6DF55A5DD56F",
           tenant_id: 'FF2B49B3-57ED-486C-8326-53DF5BA5B5B4'
         })
       )
-       window.location.href = '/form-builder';
     }
-  }, [])
+  }, []);
+  
+  useEffect(() => {
+    if (userId == "A2DEC207-EDFA-4619-BCF1-6DF55A5DD56F") {
+      window.location.href = '/form-builder';
+    }
+  }, [userId]);
 
   return (
     <div className="min-h-[82vh] flex flex-col justify-center items-center cursor-wait">
